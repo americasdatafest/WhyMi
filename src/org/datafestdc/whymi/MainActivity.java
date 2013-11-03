eff0cac5981f14a6ecb5f171b397e675c6d82887
medale <medale94@yahoo.com>
2013-11-02 20:46:37 -0400

    Added primary reason spinner

--- src/org/datafestdc/whymi/MainActivity.java
+++ src/org/datafestdc/whymi/MainActivity.java
@@ -1,34 +1,43 @@
 package org.datafestdc.whymi;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Spinner spinner = (Spinner) findViewById(R.id.primaryReasonList);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.primaryReasonList, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void saveFields(View view) {
-    	
-    }
-    
-    public void resetFields(View view) {
-    	//EditText genderText = (EditText) findViewById(R.id.genderText);
-    	//genderText.setText("");
-    }
-    
+	public void saveFields(View view) {
+
+	}
+
+	public void resetAction(View view) {
+		// EditText genderText = (EditText) findViewById(R.id.genderText);
+		// genderText.setText("");
+	}
+
 }
